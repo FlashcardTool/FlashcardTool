04cc28eaddb5f0eaf407d87f682f56d514de5b8d
JBuckley15 <37307658+JBuckley15@users.noreply.github.com>
2018-03-21 21:41:21 -0700

    Removed changes from test commit

--- app/src/main/java/com/tool/flashcard/flashcardtool/StudyModeTop.java
+++ app/src/main/java/com/tool/flashcard/flashcardtool/StudyModeTop.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 public class StudyModeTop extends AppCompatActivity {
-//
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
