43c9af7b99dc7a86378f89b7e42e693298244f63
Joshua Masci <Iamshortman@gmail.com>
2018-04-01 18:27:11 -0700

    Reworked The Main Menu
    Add and delete deck functionality still need to be implemented

--- app/src/main/java/com/tool/flashcard/flashcardtool/DeckSelect.java
+++ app/src/main/java/com/tool/flashcard/flashcardtool/DeckSelect.java
@@ -1,13 +1,11 @@
 package com.tool.flashcard.flashcardtool;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.AdapterView;
-import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 
 import com.tool.flashcard.flashcardtool.FlashCardUtilities.Deck;
@@ -16,48 +14,105 @@ import com.tool.flashcard.flashcardtool.FlashCardUtilities.DeckListAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuItem;
+import android.view.MenuInflater;
+
 public class DeckSelect extends AppCompatActivity
 {
-    public static List<Deck> Manager;
-    public static int        CurrentDeckIndex = 0;
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        Manager = new ArrayList<>();
-        Deck deck = new Deck("Test Deck");
-        Manager.add(deck);
+    private static DeckSelect instance;
 
-        for(int ind = 0; ind < 45; ind++)
-        {
-            deck.CreateNewCard("Front of card " + Integer.toString(ind), "Back of card " + Integer.toString(ind));
+    public static List<Deck> Manager;
+    public static int CurrentDeckIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        instance = this;
+        Manager = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            Deck deck = new Deck("Test Deck " + i);
+            Manager.add(deck);
+
+            for (int ind = 0; ind < 5; ind++) {
+                deck.CreateNewCard("Front of card " + Integer.toString(ind), "Back of card " + Integer.toString(ind));
+            }
         }
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deck_select);
 
-        /*ListView list = findViewById(R.id.mobile_list);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
-            {
-                Intent intent = new Intent(DeckSelect.this, DeckSelectSubMenu.class);
-                startActivity(intent);
-            }
-        });*/
-
         FloatingActionButton fab = findViewById(R.id.NewDeckButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                Intent intent = new Intent(DeckSelect.this, DeckSelectNewDeck.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                //Intent intent = new Intent(DeckSelect.this, DeckSelectNewDeck.class);
+                //startActivity(intent);
             }
         });
 
-
-        RecyclerView deck_list = (RecyclerView) findViewById(R.id.DeckList);
+        RecyclerView deck_list = findViewById(R.id.DeckList);
         deck_list.setLayoutManager(new LinearLayoutManager(this));
         deck_list.setAdapter(new DeckListAdapter());
+
+        registerForContextMenu(deck_list);
+    }
+
+    public static void onItemClick(View view, int deck_id, boolean long_click)
+    {
+        DeckSelect.CurrentDeckIndex = deck_id;
+
+        if(!long_click)
+        {
+            instance.openContextMenu(view);
+        }
+        /*String string = deck_id + ": ";
+        string += long_click ? "Long" : "Short";
+        Toast.makeText(instance, string, Toast.LENGTH_SHORT).show();*/
+
+        //Intent intent = new Intent(instance, DeckSelectSubMenu.class);
+        //instance.startActivity(intent);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        //menu.setHeaderTitle(DeckSelect.Manager.get(DeckSelect.CurrentDeckIndex).GetName());
+        /*menu.add(0, v.getId(), 0, "Study");
+        menu.add(0, v.getId(), 0, "Quiz");
+        menu.add(0, v.getId(), 0, "Edit");
+        menu.add(0, v.getId(), 0, "Export");*/
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.deck_select_sub_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        Intent intent;
+        int id = item.getItemId();
+        switch (id)
+        {
+            case R.id.sub_menu_study:
+                intent = new Intent(this, StudyMode.class);
+                startActivity(intent);
+                break;
+            case R.id.sub_menu_quiz:
+                intent = new Intent(this, QuizModeQuestion.class);
+                startActivity(intent);
+                break;
+            case R.id.sub_menu_edit:
+                intent = new Intent(this, EditMode.class);
+                startActivity(intent);
+                break;
+            case R.id.sub_menu_export:
+                break;
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/tool/flashcard/flashcardtool/FlashCardUtilities/DeckListAdapter.java
+++ app/src/main/java/com/tool/flashcard/flashcardtool/FlashCardUtilities/DeckListAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.tool.flashcard.flashcardtool.DeckSelect;
@@ -16,7 +17,7 @@ import com.tool.flashcard.flashcardtool.R;
 public class DeckListAdapter extends RecyclerView.Adapter<DeckListAdapter.ViewHolder>
 {
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    public ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType)
     {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
 
@@ -24,24 +25,47 @@ public class DeckListAdapter extends RecyclerView.Adapter<DeckListAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position)
+    public void onBindViewHolder(final ViewHolder holder, int position)
     {
         Deck deck = DeckSelect.Manager.get(position);
 
+        holder.deck_id = position;
         holder.DeckName.setText(deck.GetName());
         holder.Desc.setText("Stuff that I haven't decided what to do with");
+
+        holder.layout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                DeckSelect.onItemClick(v, holder.deck_id, false);
+            }
+        });
+
+        holder.layout.setOnLongClickListener(new View.OnLongClickListener()
+        {
+            @Override
+            public boolean onLongClick(View v)
+            {
+                DeckSelect.onItemClick(v, holder.deck_id, true);
+                return true;
+            }
+        });
     }
 
     @Override
     public int getItemCount()
     {
-        return DeckSelect.Manager.size();
+        int item_count = DeckSelect.Manager.size();
+        return item_count;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder
     {
         public TextView DeckName;
         public TextView Desc;
+        public LinearLayout layout;
+        public int deck_id;
 
         public ViewHolder(View itemView)
         {
@@ -49,6 +73,7 @@ public class DeckListAdapter extends RecyclerView.Adapter<DeckListAdapter.ViewHo
 
             this.DeckName = itemView.findViewById(R.id.textViewDeckName);
             this.Desc = itemView.findViewById(R.id.textViewDesc);
+            this.layout = itemView.findViewById(R.id.DeckListItemLayout);
         }
     }
 }
